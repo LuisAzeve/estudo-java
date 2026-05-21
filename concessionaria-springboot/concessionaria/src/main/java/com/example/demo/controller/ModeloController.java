364b7339ac36194f2006ebfcf13d4531756ba71f
luis  felippe ribeiro azevedo <159429531+LuisAzeve@users.noreply.github.com>
2026-05-21 01:50:28 -0300

    Remove comments replacing old method names in ModeloController

--- concessionaria-springboot/concessionaria/src/main/java/com/example/demo/controller/ModeloController.java
+++ concessionaria-springboot/concessionaria/src/main/java/com/example/demo/controller/ModeloController.java
@@ -14,19 +14,19 @@ public class ModeloController {
     @Autowired
     private ModeloRepository repository;
 
-    // Substitui consultarModelos()
+    
     @GetMapping
     public List<Modelo> listar() {
         return repository.findAll();
     }
 
-    // Substitui cadastrarModelo()
+    
     @PostMapping
     public Modelo cadastrar(@RequestBody Modelo modelo) {
         return repository.save(modelo);
     }
 
-    // Substitui deletarModelo()
+    
     @DeleteMapping("/{id}")
     public void deletar(@PathVariable int id) {
         repository.deleteById(id);
